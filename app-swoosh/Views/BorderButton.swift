20c7af934df89f3b1c2cd82899b1add6ddbeba37
Rana Ahmed <rana.ahmed@enozom.com>
2017-11-21 16:23:30 +0200

    create design of 2 pages

---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,27 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Hazem Mohamed Magdy on 11/21/17.
+//  Copyright © 2017 Rana. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 2.0
+        layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+}
