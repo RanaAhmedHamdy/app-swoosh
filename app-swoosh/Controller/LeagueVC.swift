db6d9697ca1e191a3d1b3839cb78a669c4714314
Rana Ahmed <rana.ahmed@enozom.com>
2017-11-22 10:21:52 +0200

    perform segue programmatically

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -16,12 +16,10 @@ class LeagueVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onNextBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-
     /*
     // MARK: - Navigation
 
